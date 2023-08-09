f53d36538b0c0b5f90df9253052a2b1b926bf37d
Ilias Rentzeperis <ilias.rentzeperis@gmail.com>
2023-08-09 20:38:50 +0300

    Add files via upload
    updated the code of jupyter and .py files

--- generativeModels.py
+++ generativeModels.py
@@ -1,5 +1,8 @@
-# Implementationof the sparse coding algorithm with different regularizers 
-# Used part of github code for the general minimization of Phi and r from the user [takyamamoto](https://github.com/takyamamoto/SparseCoding-OlshausenField-Model) 
+
+
+
+# Implementations of the sparse coding algorithm from [Olshausen & Field, *Nature* 1996]. 
+# Parts of the code were from the user [takyamamoto](https://github.com/takyamamoto/SparseCoding-OlshausenField-Model) 
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,9 +15,9 @@ import utils
 ### Have the different thresholding functions as a separate class and let the other classes use them via composition
 class Thresholding:
 
-    def __init__(self,lrR,numNonZeroR=20):
+    def __init__(self,lr_r,numNonZeroR=20):
 
-        self.lrR = lrR
+        self.lr_r = lr_r
         self.numNonZeroR = numNonZeroR
 
     #DIFFERENT THRESHOLDING FUNCTIONS
@@ -56,20 +59,21 @@ class Thresholding:
     #from Luca's Master student Marta Lazzaretti
     def CEL0Thresholding(self,r,threshold):
         a = 1
-        num = (np.abs(r) - math.sqrt(2*threshold)*a*self.lrR)
+        num = (np.abs(r) - math.sqrt(2*threshold)*a*self.lr_r)
         num[num<0] = 0
-        den = 1-a**2*self.lrR
-        return np.sign(r)*np.minimum(np.abs(r),np.divide(num,den))*(a**2*self.lrR<1)
-
+        den = 1-a**2*self.lr_r
+        return np.sign(r)*np.minimum(np.abs(r),np.divide(num,den))*(a**2*self.lr_r<1)
 
 
 
 ##########GENERAL FUNCTION used for both sparse classes
 def calculateError(error):
-    recon_error = np.mean(error**2)
+    reconError = np.mean(error**2)
     #sparsity_r = self.lmda*np.mean(np.abs(self.r)) 
     #return recon_error + sparsity_r
-    return recon_error
+    return reconError
+
+
 
 
 ### Network implementation
@@ -80,12 +84,20 @@ def calculateError(error):
 
 #FINDS BOTH PHI AND R
 class SparseModel:
-    def __init__(self, numInputs, numUnits, batchSize,lmda=5e-3,flagMethod = 'soft',lrR=1e-2, lrPhi=1e-2):
+    def __init__(self, numInputs, numUnits, batchSize,lmda,flagMethod, lr_r=1e-2, lr_Phi=1e-2):
 
-        self.lrR = lrR # learning rate of r
-        self.lrPhi = lrPhi # learning rate of Phi
+        self.lr_r = lr_r # learning rate of r
+        self.lr_Phi = lr_Phi # learning rate of Phi
+        self.numIterations = 5000
+        self.decayRateR  = self.lr_r/50
+        self.decayRatePhi = self.lr_Phi/50
+        iterationsVec = np.arange(self.numIterations)
+        self.lr_rV = (1/(1+self.decayRateR*iterationsVec))*self.lr_r
+        self.lrPhiV = (1/(1+self.decayRatePhi*iterationsVec))*self.lr_Phi
+        self.iterPhi = 0
+
         self.lmda = lmda # regularization parameter
-        self.threshold = self.lmda*self.lrR
+        self.threshold = self.lmda*self.lr_r
         #self.threshold = self.lmda
 
         self.numInputs = numInputs
@@ -93,12 +105,12 @@ class SparseModel:
         self.batchSize = batchSize
         self.flagMethod = flagMethod
 
-        self.objThreshold = Thresholding(self.lrR)
+        self.objThreshold = Thresholding(self.lr_r)
 
-        self.counterPhi = 0; self.PhiDict = {}
         # Weights
         Phi = np.random.randn(self.numInputs, self.numUnits).astype(np.float32)
         self.Phi = Phi * np.sqrt(1/self.numUnits)
+        
     
     def initializeStates(self,rInit):
         #it will be of size batchSizeXnumUnits
@@ -110,74 +122,29 @@ class SparseModel:
         #so basically the phi vectors are normalized to unit vectors
         self.Phi = self.Phi / np.maximum(np.linalg.norm(self.Phi, ord=2, axis=0, keepdims=True), 1e-8)
 
-    def __call__(self, inputs, training=True):
+    #we run a learning decay of the learning rates for r and Phi so we need the number of iterations: iterR AND iterPhi
+    def __call__(self, inputs, iterR,training=True):
         # Updates  
-        #print(f'the size of gain is {self.gain.shape}')
-        #print(f'the size of phi is {self.Phi.shape}')
-        #print(f'the size of r is {self.r.shape}')
-        #print(f'the size of inputs is {inputs.shape}')
+
         error = inputs - self.r @ self.Phi.T
-        r = self.r + self.lrR * error @ self.Phi
+        r = self.r + self.lr_rV[iterR] * error @ self.Phi
         
         if self.flagMethod == 'soft':
             self.r = self.objThreshold.softΤhresholding(r, self.threshold)
         elif self.flagMethod == 'hard1':
-            self.r = self.objThreshold.hardΤhresholding1(r, self.threshold)
+            self.r = self.objThreshold.hardThresholding1(r, self.threshold)
         elif self.flagMethod == 'half':
-            self.r = self.objThreshold.halfΤhresholding(r,self.threshold)
+            self.r = self.objThreshold.halfThresholding(r,self.threshold)
         elif self.flagMethod == 'CEL0':
             self.r = self.objThreshold.CEL0Thresholding(r,self.lmda)
 
         if training:  
+            #print(f'iteration for learning Phi is {self.iterPhi} and the learning rate of phi is {self.lrPhiV[self.iterPhi]}')
             error = inputs - self.r @ self.Phi.T
             dPhi = error.T @ self.r
-            self.Phi += self.lrPhi * dPhi
-    
-            self.counterPhi = self.counterPhi+1
-            #if self.counterPhi%10 == 0:
-            #    self.PhiDict[self.counterPhi] = self.Phi
-           
-        return error, self.r
+            self.Phi += self.lrPhiV[self.iterPhi] * dPhi
+            self.iterPhi +=1
 
-
-#### Variation of the model above. The class below finds the sparse activities with fixed Phi
-class SparseModelFixedPhi:
-    def __init__(self,Phi, batchSize, lrR, lmda,flagMethod = 'soft'):
-
-        self.lrR = lrR # learning rate of r
-        self.lmda = lmda # regularization parameter
-        self.threshold = self.lmda*self.lrR
-        #self.threshold = self.lmda
-
-        self.batchSize = batchSize
-        self.numInputs = Phi.shape[0]
-        self.numUnits = Phi.shape[1]
-        self.flagMethod = flagMethod
-
-        self.objThreshold = Thresholding(self.lrR)
-
-        # activity of neurons
-        self.r = np.zeros((self.batchSize, self.numUnits))
-        self.Phi = Phi
-    
-    def initializeStates(self,rInit):
-        #it will be of size batchSizeXnumUnits
-        self.r = rInit
-        #print(np.sum(self.r))
-        
-    def __call__(self, inputs):
-        # Updates                
-        error = inputs - self.r @ self.Phi.T     
-        r = self.r + self.lrR * error @ self.Phi
-        
-        if self.flagMethod == 'soft':
-            self.r = self.objThreshold.softΤhresholding(r, self.threshold)
-        elif self.flagMethod == 'hard1':
-            self.r = self.objThreshold.hardΤhresholding1(r, self.threshold)
-        elif self.flagMethod == 'half':
-            self.r = self.objThreshold.halfΤhresholding(r,self.threshold)
-        elif self.flagMethod == 'CEL0':
-            self.r = self.objThreshold.CEL0Thresholding(r,self.lmda)
             
         return error, self.r
 
@@ -205,21 +172,22 @@ def runModelSim(model,numIter,batchSize,inputsAll,rAll, ntMax = 5000,eps = 1e-2)
         model.normalizeRows() # Normalize weights
     
         # Input an image patch until latent variables are converged 
-        rTemp = model.r # set previous r (t minus 1)
+        rTm1 = model.r # set previous r (t minus 1)
 
         for t in range(ntMax):
             # Update r without update weights 
-            error, r = model(inputs, training=False)
-            dr = r - rTemp 
+            error, r = model(inputs, t,training=False)
+            dr = r - rTm1 
 
             # Compute norm of r
-            drNorm = np.linalg.norm(dr, ord=2) / (eps + np.linalg.norm(rTemp, ord=2))
-            rTemp = r # update rTemp
+            drNorm = np.linalg.norm(dr, ord=2) / (eps + np.linalg.norm(rTm1, ord=2))
+            rTm1 = r # update rTm1
         
             # Check convergence of r, then update weights
             if drNorm < eps:
                 #after the r's batch converges, you update the phi
-                error, r = model(inputs, training=True)
+                
+                error, r = model(inputs, t,training=True)
                 rAll_.append(r)
                 break
         
@@ -235,53 +203,23 @@ def runModelSim(model,numIter,batchSize,inputsAll,rAll, ntMax = 5000,eps = 1e-2)
         if iter_ % 100 == 99:  
             print("iter: "+str(iter_+1)+"/"+str(numIter)+", Moving error:",
                   np.mean(errorList[iter_-99:iter_]))
-            
-       
-    return model,rAll_, errorList      
+
+    #return model,rAll_, errorList       
+    #############################################################
+    rAll = np.array(rAll_)
+    print(f'r shape is initially {rAll.shape}')
+    rAll = np.reshape(rAll,(rAll.shape[0]*rAll.shape[1],-1))
+    print(f'... and now {rAll.shape}')
+    
+    rActivityFreqUnit = np.sum(rAll!=0,axis=0)/rAll.shape[0]
+    print('did the operation for rActivityFreqUnit')
+    rNumActiveInstances = np.sum(rAll!=0,axis=1,keepdims = True)
+    print('did the operation for rNumActiveInstances')
+    errorRates = np.array(errorList)
+    print('did the operation for errorRates')
+
+    return model.Phi,rActivityFreqUnit, rNumActiveInstances, errorRates     
     
 
-## Function running simulation for SparseModelfixedPhi
-#The for loop updates until r converges
-#ntMax = maximum number of iterations for convergence
-#eps  small value which determines convergence
-def runModelSimFixedPhi(model,numIter,batchSize,inputsAll,rAll, ntMax = 5000,eps = 1e-2):
-    
-    errorList = [] # List to save errors
-    rAll_ = [] #gather all r to do the analysis 
-    
-    # Run simulation
-    for iter_ in tqdm(range(numIter)):
-        
-        inputs = inputsAll[iter_*batchSize:(iter_+1)*batchSize,:] # Input image patches
-        rInit = rAll[iter_*batchSize:(iter_+1)*batchSize,:]
-
-        model.initializeStates(rInit) # Reset r's
-    
-        # Input an image patch until latent variables are converged 
-        rTemp = model.r # set previous r (t minus 1)
-
-        for t in range(ntMax):
-            # Update r without update weights 
-            error, r = model(inputs)
-            dr = r - rTemp 
-
-            # Compute norm of r
-            drNorm = np.linalg.norm(dr, ord=2) / (eps + np.linalg.norm(rTemp, ord=2))
-            rTemp = r # update rTemp
-        
-            # Check convergence of r, then update weights
-            if drNorm < eps:
-                #after the r's batch converges, you update the phi
-                error, r = model(inputs)
-                rAll_.append(r)
-                errorList.append(calculateError(error)) # Append errors
-                break
-        
-            # If failure to convergence, break and print error
-            if t >= ntMax-2: 
-                print(f'Error at patch: {iter_}. The norm is {drNorm}')
-                break
-                
-    return model,rAll_, errorList      
 
 
